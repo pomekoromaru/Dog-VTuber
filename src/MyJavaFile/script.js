633c25017dd728b0b219feb7f8f3a5774d6e8e2a
pomekoromaru <pomekoromaru@gmail.com>
2024-12-14 01:02:42 +0900

    Add files via upload

--- src/MyJavaFile/script.js
+++ src/MyJavaFile/script.js
@@ -1,13 +1,33 @@
+// 初期化時に最初のセクションを表示する
+document.addEventListener('DOMContentLoaded', () => {
+    const defaultSection = document.querySelector('.content:not(.hidden)');
+    const sectionButton = document.querySelector('.mobile-nav button');
+    if (defaultSection && sectionButton) {
+        sectionButton.textContent = defaultSection.querySelector('h1').textContent;
+    }
+});
+
+// ドロップダウンを開閉する
 function toggleDropdown() {
     const dropdown = document.querySelector('.mobile-nav .dropdown');
     dropdown.classList.toggle('show');
 }
 
+// セクションを切り替える
 function navigate(sectionId) {
-    // セクションの表示切り替え
+    // 全セクションを非表示にする
     const sections = document.querySelectorAll('.content');
     sections.forEach(section => section.classList.add('hidden'));
-    document.getElementById(sectionId).classList.remove('hidden');
+
+    // 対象セクションを表示
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        targetSection.classList.remove('hidden');
+    }
+
+    // 現在のセクション名をボタンに表示
+    const sectionButton = document.querySelector('.mobile-nav button');
+    sectionButton.textContent = targetSection.querySelector('h1').textContent;
 
     // ドロップダウンを閉じる
     const dropdown = document.querySelector('.mobile-nav .dropdown');
